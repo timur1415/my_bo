4c91ab889ffe603b2af07d51d2250b3c68727aca
Timur_Maksudov <timurmakssudov@gmail.com>
2025-03-09 18:16:30 +0300

    теперь точно закончил всё с урока 9 марта

--- knb_game.py
+++ knb_game.py
@@ -8,7 +8,7 @@ from telegram.ext import (
 import random
 
 # from start import start
-
+from db import update_wins_knb
 
 
 
@@ -53,21 +53,27 @@ async def knb_game(update: Update, context: ContextTypes.DEFAULT_TYPE):
             chat_id=update.effective_chat.id,
             text="красава победа на твоей стороне!!!\n\nя выбрал ножницы",
         )
+        update_wins_knb(id)
+        
     elif text_user == "ножницы" and comp_hod[n] == "бумага":
         await context.bot.send_message(
             chat_id=update.effective_chat.id,
             text="красава победа на твоей стороне!!!\n\nя выбрал бумагу",
         )
+        update_wins_knb(id)
     elif text_user == "бумага" and comp_hod[n] == "камень":
         await context.bot.send_message(
             chat_id=update.effective_chat.id,
             text="красава победа на твоей стороне!!!\n\nя выбрал камень ",
         )
+        update_wins_knb(id)
     elif text_user == "лом":
         await context.bot.send_message(
             chat_id=update.effective_chat.id,
             text="против лома нет приёма!!!\n\nПОСХАЛКА!!!",
         )
+        update_wins_knb(id)
+        
     elif text_user == 'выход':
         return MAIN_MENU
 
