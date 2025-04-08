45fd5b6fec83728f18d7f17455cdad5f28e32e85
Timur_Maksudov <timurmakssudov@gmail.com>
2025-04-08 22:21:11 +0300

    добавил личную стату кнб

--- db.py
+++ db.py
@@ -1,13 +1,14 @@
 import sqlite3
 
+
 def create_table():
     # Подлючаемся к бдхе
-    conn = sqlite3.connect('game_bot.db')
+    conn = sqlite3.connect("game_bot.db")
     # Создать курсор
     cur = conn.cursor()
 
     # Выполняем какую-то команду
-    cur.execute('''CREATE TABLE IF NOT EXISTS users(
+    cur.execute("""CREATE TABLE IF NOT EXISTS users(
                     id INTEGER PRIMARY KEY,
                     name TEXT,
                     knb_win INTEGER,
@@ -15,34 +16,41 @@ def create_table():
                     bac_record INTEGER,
                     knz_win_x INTEGER,
                     knz_win_O
-    )''')
+    )""")
 
     # Сохранить если было изменение
     conn.commit()
     # Подключение закрыть
     conn.close()
 
-def create__user(id, name):
-     # Подлючаемся к бдхе
-    conn = sqlite3.connect('game_bot.db')
-    # Создать курсор
-    cur = conn.cursor()
-    cur.execute(f'SELECT id FROM users WHERE id={id}')
-    user_data = cur.fetchone()#вернётся либо none  | (123) 
-    if user_data == None:
-        cur.execute(f'INSERT INTO users VALUES({id}, "{name}", 0, 0, 0, 0, 0)')
-        conn.commit()
-    conn.close() 
 
-def update_wins_knb(id,result):
+def create__user(id, name):
     # Подлючаемся к бдхе
-    conn = sqlite3.connect('game_bot.db')
+    conn = sqlite3.connect("game_bot.db")
     # Создать курсор
     cur = conn.cursor()
-    if result == 'w':
-        cur.execute(f'UPDATE users SET knb_win = knb_win + 1, knb_games = knb_games + 1 WHERE id = {id}')
+    cur.execute(f"SELECT id FROM users WHERE id={id}")
+    user_data = cur.fetchone()  # вернётся либо none  | (123)
+    if user_data == None:
+        cur.execute(
+            f'INSERT INTO users VALUES({id}, "{name}", 0, 0, 0, 0, 10000000000000)'
+        )
+        conn.commit()
+    conn.close()
+
+
+def update_wins_knb(id, result):
+    # Подлючаемся к бдхе
+    conn = sqlite3.connect("game_bot.db")
+    # Создать курсор
+    cur = conn.cursor()
+    if result == "w":
+        cur.execute(
+            f"UPDATE users SET knb_win = knb_win + 1, knb_games = knb_games + 1 WHERE id = {id}"
+        )
     else:
-        cur.execute(f'UPDATE users SET knb_games = knb_games + 1 WHERE id = {id}')
+        cur.execute(f"UPDATE users SET knb_games = knb_games + 1 WHERE id = {id}")
+
     conn.commit()
     # Подключение закрыть
     conn.close()
@@ -50,11 +58,11 @@ def update_wins_knb(id,result):
 
 def get_knb_rate():
     lst = []
-     # Подлючаемся к бдхе
-    conn = sqlite3.connect('game_bot.db')
+    # Подлючаемся к бдхе
+    conn = sqlite3.connect("game_bot.db")
     # Создать курсор
     cur = conn.cursor()
-    cur.execute('SELECT name, knb_win, knb_games FROM users')
+    cur.execute("SELECT name, knb_win, knb_games FROM users")
     data = cur.fetchall()
     for user in data:
         name = user[0]
@@ -64,19 +72,19 @@ def get_knb_rate():
             proc = (wins / games) * 100
         else:
             proc = 0
-        lst.append([name,proc])
-    lst.sort(reverse = True)
+        lst.append([name, proc])
+    lst.sort(reverse=True)
     return lst
 
+
 def update_bac_record(id, record):
-    conn = sqlite3.connect('game_bot.db')
+    conn = sqlite3.connect("game_bot.db")
     cur = conn.cursor()
-    cur.execute(f'SELECT id, bac_record from users WHERE id={id}')
+    cur.execute(f"SELECT id, bac_record from users WHERE id={id}")
     data = cur.fetchone()
     id = data[0]
     bac_record = data[1]
     if record < bac_record:
-        cur.execute(f'UPDATE users SET bac_record = {record} WHERE id={id}')
+        cur.execute(f"UPDATE users SET bac_record = {record} WHERE id={id}")
     conn.commit()
     conn.close()
-
--- rate.py
+++ rate.py
@@ -12,35 +12,47 @@ from states import (
     RATE,
 )
 
+
 async def my_stat(update: Update, context: ContextTypes.DEFAULT_TYPE):
     query = update.callback_query
     await query.answer()
     keyboard = [
-        [InlineKeyboardButton("топ игроков по проценту в камень ножницы бумага", callback_data="knb_stat")],
+        [
+            InlineKeyboardButton(
+                "топ игроков по проценту в камень ножницы бумага",
+                callback_data="knb_stat",
+            )
+        ],
         [InlineKeyboardButton("рекорд в быки и коровы", callback_data="bac_stat")],
         [InlineKeyboardButton("стата крестики нолики", callback_data="knz_stat")],
         [InlineKeyboardButton("назад", callback_data="back")],
     ]
 
     markup = InlineKeyboardMarkup(keyboard)
-
-    await query.edit_message_text("ваша стата", reply_markup=markup)
+    procent = get_knb_rate()
+    for user in procent:
+        numbers = round(user[1])
+    await query.edit_message_text(
+        f"привет, {update.effective_user.first_name}, ваша статистика:\n\nкамень ножницы бумага:\nваш процент побед состовляет {numbers}% ",
+        reply_markup=markup,
+    )
 
     return RATE
 
 
 async def knb_stat(update: Update, context: ContextTypes.DEFAULT_TYPE):
     procent = get_knb_rate()
-    text = ''
+    text = ""
     for user in procent:
         numbers = round(user[1], 1)
-        text += f'{user[0]} -- {numbers}%\n\n'
+        text += f"{user[0]} -- {numbers}%\n\n"
     query = update.callback_query
     keyboard = [[InlineKeyboardButton("назад", callback_data="back")]]
 
     markup = InlineKeyboardMarkup(keyboard)
 
-    await query.edit_message_text(f'{text}', reply_markup=markup)
+    await query.edit_message_text(f"{text}", reply_markup=markup)
+
 
 async def bac_stat(update: Update, context: ContextTypes.DEFAULT_TYPE):
     query = update.callback_query
@@ -48,7 +60,8 @@ async def bac_stat(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     markup = InlineKeyboardMarkup(keyboard)
 
-    await query.edit_message_text('ваша стата в быки и коровы', reply_markup=markup)
+    await query.edit_message_text("ваша стата в быки и коровы", reply_markup=markup)
+
 
 async def knz_stat(update: Update, context: ContextTypes.DEFAULT_TYPE):
     query = update.callback_query
@@ -56,4 +69,4 @@ async def knz_stat(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     markup = InlineKeyboardMarkup(keyboard)
 
-    await query.edit_message_text('ваша стата в быки и коровы', reply_markup=markup)
+    await query.edit_message_text("ваша стата в быки и коровы", reply_markup=markup)
